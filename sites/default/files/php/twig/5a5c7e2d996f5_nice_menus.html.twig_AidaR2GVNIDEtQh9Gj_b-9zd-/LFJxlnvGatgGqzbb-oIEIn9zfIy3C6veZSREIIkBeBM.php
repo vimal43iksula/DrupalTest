cba2ea2f16629b09c0007b3a94a17b7e2d37bbb3
vimal <vimal.iksula@gmail.com>
2018-01-19 10:49:32 +0530

    Uploaded files of demo sites

---
+++ sites/default/files/php/twig/5a5c7e2d996f5_nice_menus.html.twig_AidaR2GVNIDEtQh9Gj_b-9zd-/LFJxlnvGatgGqzbb-oIEIn9zfIy3C6veZSREIIkBeBM.php
@@ -0,0 +1,73 @@
+<?php
+
+/* modules/contrib/nice_menus/templates/nice_menus.html.twig */
+class __TwigTemplate_366b3b0f8cdfedf98e1327f21464edba31eb2c7cd78a978b05e805fc3972b415 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["menu_output"]) ? $context["menu_output"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/nice_menus/templates/nice_menus.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/contrib/nice_menus/templates/nice_menus.html.twig", "/var/www/html/drupal/modules/contrib/nice_menus/templates/nice_menus.html.twig");
+    }
+}
